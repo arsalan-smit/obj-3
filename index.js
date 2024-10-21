cc8f9f375fa2db6a85936094aeef935c0c0da5c6
arsalan-smit <arsalangg05@gmail.com>
2024-10-21 18:19:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var age = 20;
+alert(`i am ${age} years old`);
+
+var visit = 5;
+alert(`You have visited this site ${visit} times.`);
+
+var birthYear = 2004;
+document.write(`My birth year is ${birthYear} <br>Data type of my declared variable is ${typeof(birthYear)}`);
+
+var visitorName = "Arsalan";
+var productTitle = "T-shirt's";
+var quantity = 5;
+document.write(`<br><br>${visitorName} ordered ${quantity} ${productTitle} on SMIT Clothing store.`);
